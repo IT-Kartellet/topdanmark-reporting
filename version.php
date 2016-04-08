bc565b39581c58e88d4ad613ff336740f9673d82
Henrik Thorn <henrikthorn@it-kartellet.dk>
2016-04-08 11:18:12 +0200

    Added new version

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 
-$plugin->version  = 2012050101;   // The (date) version of this plugin
+$plugin->version  = 2016040800;   // The (date) version of this plugin
 $plugin->requires = 2010021900;   // Requires this Moodle version
